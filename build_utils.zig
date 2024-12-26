f8273d7804aac47f7498dc93012e94bddfc64a03
Vesper <3692034+noodlecollie@users.noreply.github.com>
2024-12-26 15:36:38 +0000

    Wrapped C target compilation for convenience

--- build_utils.zig
+++ build_utils.zig
@@ -12,6 +12,95 @@
 
 const std = @import("std");
 
+pub const CDep = struct {
+    /// Struct representing a compilation target comprised of C code.
+    pub const CompileTarget = struct {
+        /// Root directory of the dependency's repo.
+        root: std.Build.LazyPath,
+
+        /// Directories that users of the artifact should
+        /// look in for header files.
+        include_subdirs: []const []const u8,
+
+        /// Target within the build system.
+        target: *std.Build.Step.Compile,
+
+        // Given an existing target created using eg. `addStaticLibrary()`,
+        // and a build configuration, adds the relevant C source files to
+        // and include paths to the target.
+        pub fn init(b: *std.Build, existing_target: *std.Build.Step.Compile, config: anytype) CompileTarget {
+            const concrete_config = Config.init(config);
+
+            const c_target: CompileTarget = .{
+                .root = b.dependency(concrete_config.dependency_name, .{}).path(""),
+                .include_subdirs = concrete_config.include_subdirs,
+                .target = existing_target,
+            };
+
+            inline for (c_target.include_subdirs) |subdir| {
+                c_target.target.addIncludePath(c_target.root.path(b, subdir));
+            }
+
+            const source_root = c_target.root.path(b, if (concrete_config.source_root) concrete_config.source_root else "");
+
+            c_target.addCSourceFiles(.{
+                .root = source_root,
+                .files = &concrete_config.source_files,
+                .flags = &concrete_config.compile_flags,
+            });
+
+            c_target.linkLibC();
+
+            return c_target;
+        }
+
+        // Given a `CompileTarget` initialised earlier, sets up `other` to link against it.
+        pub fn link(this: CompileTarget, b: *std.Build, other: *std.Build.Step.Compile) void {
+            inline for (this.include_subdirs) |subdir| {
+                other.addIncludePath(this.root.path(b, subdir));
+            }
+
+            other.linkLibrary(this);
+            other.linkLibC();
+        }
+    };
+
+    // Internal:
+    const Config = struct {
+        dependency_name: []const u8,
+        include_subdirs: []const []const u8,
+        source_root: ?[]const []const u8,
+        source_files: []const []const u8,
+        compile_flags: []const []const u8,
+
+        // The config is intended to be the result of @import("...)".
+        // We need to check that it exposes the properties that we need.
+        pub fn init(config: anytype) Config {
+            return .{
+                .dependency_name = checkFieldType(config, "dependency_name", []const u8),
+                .include_subdirs = checkFieldType(config, "include_subdirs", []const []const u8),
+                .source_root = checkFieldType(config, "source_files", ?[]const []const u8),
+                .source_files = checkFieldType(config, "source_files", []const []const u8),
+                .compile_flags = checkFieldType(config, "compile_flags", []const []const u8),
+            };
+        }
+
+        fn checkFieldType(config: anytype, comptime field_name: []const u8, comptime T: type) T {
+            const field = @field(config, field_name);
+            const field_type = @TypeOf(field);
+
+            comptime {
+                if (field_type != T) {
+                    @panic("Expected type " ++ @typeName(config) ++ " to contain field " ++ field_name ++
+                        " of type " ++ @typeName(T) ++ ", but it was of type " ++ @typeName(field_type));
+                }
+            }
+
+            return field;
+        }
+    };
+};
+
 /// Finds a module from a dependency, and imports it into the provided local module.
 /// `b` is the current Zig build object.
 /// `dep_name` specifies the name of the dependency (as per `build.zig.zon`).
---
+++ docs/zig-reference/compiling-and-linking-a-c-library.md
@@ -0,0 +1,122 @@
+This page assumes a static C library. Other targets should be similar in nature. This example uses the [cJSON library](https://github.com/DaveGamble/cJSON).
+
+## Add The Repo As A Dependency
+
+Firstly, determine the URL to the desired archive of the repository, preferably at a specific tag. This should be a `.tar.gz`. An example URL might be `https://github.com/DaveGamble/cJSON/archive/refs/tags/v1.7.18.tar.gz`
+
+Run `zig fetch <url>`, which will print out the hash for the repo's archive. Use this to add the repo to `build.zig.zon` as follows:
+
+```
+.{
+    .dependencies = .{
+        .cJSON = .{
+            .url = "https://github.com/DaveGamble/cJSON/archive/refs/tags/v1.7.18.tar.gz",
+            .hash = "1220a3c4dfa93f6cbef20aa0cc177c7debc02ced588520dad4ab5772956ad5da1c73",
+        },
+    },
+}
+```
+
+Note that `zig fetch --save` cannot be used here, since this expects the dependent repo to be a Zig repo, and will fail if there is no `build.zig.zon` present in the dependent repo.
+
+Additionally, you can use `@"..." = .{ }` to name the dependency if its name contains disallowed character such as hyphens.
+
+## Write A Build Description File For The Library
+
+You may wish to create a directory for these build files, eg. `cdeps`. For this example, we will create `cdeps/cJSON.zig`. This file will hold a configuration used to build the library.
+
+For `cJSON`, the file looks like this:
+
+```
+// Subdirectories under the root within which header files can be found.
+pub const include_subdirs = .{
+    "",
+};
+
+// Source files to be built.
+pub const source_files = .{
+    "cJSON.c",
+    "cJSON_Utils.c",
+};
+
+// Compile flags to be applied when building.
+pub const compile_flags = .{
+    "-std=c89",
+    "-pedantic",
+    "-Wall",
+    "-Wextra",
+    "-Werror",
+    "-Wstrict-prototypes",
+    "-Wwrite-strings",
+    "-Wshadow",
+    "-Winit-self",
+    "-Wcast-align",
+    "-Wformat=2",
+    "-Wmissing-prototypes",
+    "-Wstrict-overflow=2",
+    "-Wcast-qual",
+    "-Wundef",
+    "-Wswitch-default",
+    "-Wconversion",
+    "-Wc++-compat",
+    "-fstack-protector-strong",
+    "-Wcomma",
+    "-Wdouble-promotion",
+    "-Wparentheses",
+    "-Wformat-overflow",
+    "-Wunused-macros",
+    "-Wmissing-variable-declarations",
+    "-Wused-but-marked-unused",
+    "-Wswitch-enum",
+};
+
+```
+
+## Build The Library
+
+Given this configuration, we can build the library as follows:
+
+```
+// At the top of build.zig:
+const cJSON = @import("./cdeps/cJSON.zig");
+
+// Then later, in the build() function:
+const cjson_path = b.dependency("cJSON", .{}).path("");
+
+const cjson_lib = b.addStaticLibrary(.{
+	.name = "cJSON",
+	.target = target,
+	.optimize = optimize,
+});
+
+inline for (cJSON.include_subdirs) |subdir| {
+	cjson_lib.addIncludePath(cjson_path.path(b, subdir));
+}
+
+cjson_lib.addCSourceFiles(.{
+	.root = cjson_path,
+	.files = &cJSON.source_files,
+	.flags = &cJSON.compile_flags,
+});
+
+cjson_lib.linkLibC();
+```
+
+Then, to add the library to a Zig target, do:
+
+```
+inline for (cJSON.include_subdirs) |subdir| {
+	my_target.addIncludePath(cjson_path.path(b, subdir));
+}
+
+my_target.linkLibrary(cjson_lib);
+my_target.linkLibC();
+```
+
+## Using `build_utils.zig`
+
+The above functionality has been wrapped in `build_utils.zig` for convenience. This means you can do:
+
+```
+// TODO
+```
